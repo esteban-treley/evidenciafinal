17a137ef46c637b6c2307db3d8a375908fb16c5e
Esteban Treviño <estebantrvn@gmail.com>
2020-08-08 01:29:44 -0500

    primer commit

---
+++ Evidencia/src/main/java/com/mycompany/evidencia/Doctor.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.evidencia;
+
+import java.io.File;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileWriter;
+
+/**
+ *
+ * @author esteban
+ */
+public class Doctor {
+
+    //Atributes
+    public int id;
+    public String name;
+    public String speciality;
+
+    private String fileName = "Doctor.csv";
+   
+    //Constructors
+    public Doctor() {
+    }
+
+    public Doctor(String name, String speciality) {
+        this.name = name;
+        this.speciality = speciality;
+    }
+
+    //Methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static File getDoctorFile() {
+        File here = new File(".");
+        File doctorFile = null;
+
+        try {
+            String herePath;
+            herePath = here.getCanonicalPath() + "/Doctor.csv";
+            doctorFile = new File(herePath);
+            if (!doctorFile.exists()) {
+                doctorFile.createNewFile();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return doctorFile;
+    }
+
+    public void setConsecutiveId() {
+
+        String line = null;
+        File doctorFile = getDoctorFile();
+        int countLine = 0;
+        try {
+            Scanner scanDoctorFile = new Scanner(doctorFile);
+            while (scanDoctorFile.hasNextLine()) {
+                line = scanDoctorFile.nextLine();
+                countLine++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        this.id = line != "" && countLine > 0 ? Integer.parseInt(line.split(",")[0]) + 1 : 1;
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSpeciality() {
+        return this.speciality;
+    }
+
+    public void save() {
+        String data = Integer.toString(this.id) + "," + this.name + "," + this.speciality;
+        try {
+            File file = getDoctorFile();
+            FileWriter fr = new FileWriter(file, true);
+            fr.write(data);
+            fr.write(System.lineSeparator());
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Evidencia/src/main/java/com/mycompany/evidencia/Principal.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.evidencia;
+
+import java.util.Scanner;
+import com.mycompany.evidencia.*;
+
+/**
+ *
+ * @author esteban
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        String user, password;
+        Scanner readline = new Scanner(System.in);
+
+        System.out.println("*****SISTEMA INTEGRAL DE REGISTROS MEDICOS*****");
+        System.out.println("Ingrese usuario: ");
+        user = readline.nextLine();
+        System.out.println("Ingrese contraseña: ");
+        password = readline.nextLine();
+        showMenu();
+    }
+
+    public static void showMenu() {
+        int option = 0;
+        Scanner readline = new Scanner(System.in);
+        do {
+            try {
+                System.out.println();
+                System.out.println("*****MENU*****");
+                System.out.println("1. Registrar doctor");
+                System.out.println("2. Registrar paciente");
+                System.out.println("3. Registrar cita");
+                System.out.println("4. Establecer doctor y paciente a cita");
+                System.out.println("5. Salir");
+                option = Integer.parseInt(readline.nextLine());
+
+                switch (option) {
+                    case 1:
+                        registerDoctor();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        System.out.println();
+                        System.out.println("Esta opción no existe. Por favor, elige otra.");
+                        System.out.println("********************");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println();
+                System.out.println("*****MENU*****");
+                System.out.println("1. Registrar doctor");
+                System.out.println("2. Registrar paciente");
+                System.out.println("3. Registrar cita");
+                System.out.println("4. Establecer doctor y paciente a cita");
+                System.out.println("5. Salir");
+                option = Integer.parseInt(readline.nextLine());
+
+                switch (option) {
+                    case 1:
+                        registerDoctor();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        System.out.println();
+                        System.out.println("Esta opción no existe. Por favor, elige otra.");
+                        System.out.println("********************");
+                }
+            }
+
+        } while (option != 5);
+
+    }
+
+    public static void registerDoctor() {
+        try {
+            Doctor newDoctor = new Doctor();
+            newDoctor.setConsecutiveId();
+            Scanner read = new Scanner(System.in);
+
+            System.out.print("Escriba nombre del doctor: ");
+            newDoctor.setName(read.nextLine());
+            System.out.print("Escriba especialidad del doctor: ");
+            newDoctor.setSpeciality(read.nextLine());
+
+            newDoctor.save();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
